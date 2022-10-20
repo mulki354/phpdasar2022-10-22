051ebfc88ea6a2dd995140e32fdf3a7d36fe74b3
emul <mmm.for.mlk@gmail.com>
2022-10-21 01:53:11 +0700

    menambahkan detail agar bisa melihat foto

--- latihan2/detail.php
+++ latihan2/detail.php
@@ -19,7 +19,7 @@ $mhs = query("SELECT * FROM datamhs WHERE id = $id");
 <body>
   <h3>Detail Mahasiswa</h3>
   <ul>
-    <li><img src="img/20220811192813-62f53c2ddcb07.jpeg" height="75"></li>
+    <li><img src="img/<?= $mhs["gambar"]; ?>" height="75"></li>
     <li>Nama : <?= $mhs["nama"]; ?></li>
     <li>NPM : <?= $mhs["npm"]; ?></li>
     <li>Email : <?= $mhs["email"]; ?></li>
